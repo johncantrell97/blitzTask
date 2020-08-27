2994c4137d867e15a60465bcc1eb50fa311653ff
John Cantrell <johncantrell97@gmail.com>
2020-08-27 12:23:38 -0400

    example task

---
+++ tasks/example.ts
@@ -0,0 +1,9 @@
+import exampleUtil from "utils/exampleUtil"
+
+const myTask = () => {
+    exampleUtil()
+}
+
+myTask()
+
+export default exampleUtil
---
+++ utils/exampleUtil.ts
@@ -0,0 +1,4 @@
+const exampleUtil = () => {
+    console.log("im useless but I should be logged")
+}
+export default exampleUtil
